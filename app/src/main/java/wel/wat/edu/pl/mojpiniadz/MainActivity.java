54521e27315e4f191d859e6b7e455a0b89c14bde
Paweł <pawel.barszcz@o2.pl>
2019-05-26 15:53:48 +0200

    dodanie górnych opcji

--- app/src/main/java/wel/wat/edu/pl/mojpiniadz/MainActivity.java
+++ app/src/main/java/wel/wat/edu/pl/mojpiniadz/MainActivity.java
@@ -2,6 +2,7 @@ package wel.wat.edu.pl.mojpiniadz;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_bar,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
