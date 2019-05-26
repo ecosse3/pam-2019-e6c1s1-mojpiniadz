8a49567f9882113e0d5f90e85ae92e699dc0446a
Ada <tomaszewska.ada@o2.pl>
2019-05-26 22:39:24 +0200

    poprawienie przycisków w ustawieniach

--- app/src/main/java/wel/wat/edu/pl/mojpiniadz/SettingsActivity.java
+++ app/src/main/java/wel/wat/edu/pl/mojpiniadz/SettingsActivity.java
@@ -2,7 +2,9 @@ package wel.wat.edu.pl.mojpiniadz;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,16 +13,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    TextView currency, categories,set_PIN,personal_data,about_prog;
-
+    LinearLayout currency_layout, categories_layout, set_pin_layout,personal_data_layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        currency=findViewById(R.id.currency);
-        categories=findViewById(R.id.categories);
-        set_PIN=findViewById(R.id.set_PIN);
-        personal_data=findViewById(R.id.personal_data);
+
+        currency_layout=findViewById(R.id.currency_layout);
+        categories_layout=findViewById(R.id.categories_layout);
+        set_pin_layout=findViewById(R.id.set_pin_layout);
+        personal_data_layout=findViewById(R.id.personal_data_layout);
+
 
         getSupportActionBar().setTitle("Ustawienia");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -28,16 +31,16 @@ public class SettingsActivity extends AppCompatActivity {
     public void click(View v) {
         Intent intent = new Intent();
         switch(v.getId()) {
-            case R.id.currency: // R.id.textView1
+            case R.id.currency_layout: // R.id.textView1
                 intent = new Intent(this, CurrencyActivity.class);
                 break;
-            case R.id.categories: // R.id.textView2
+            case R.id.categories_layout: // R.id.textView2
                 intent = new Intent(this, CategoryActivity.class);
                 break;
-            case R.id.set_PIN: // R.id.textView2
+            case R.id.set_pin_layout: // R.id.textView2
                 intent = new Intent(this, Set_PINActivity.class);
                 break;
-            case R.id.personal_data: // R.id.textView2
+            case R.id.personal_data_layout: // R.id.textView2
                 intent = new Intent(this, PersonalDataActivity.class);
                 break;
         }
