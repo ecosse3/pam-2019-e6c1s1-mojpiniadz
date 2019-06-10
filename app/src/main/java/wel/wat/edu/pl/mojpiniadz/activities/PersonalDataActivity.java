308fcc127ea2a3100adab0e1cdaba34456191332
Ada <tomaszewska.ada@o2.pl>
2019-06-10 21:06:26 +0200

    dodanie dodawania i modyfikacji danych użytkownika

--- app/src/main/java/wel/wat/edu/pl/mojpiniadz/activities/PersonalDataActivity.java
+++ app/src/main/java/wel/wat/edu/pl/mojpiniadz/activities/PersonalDataActivity.java
@@ -2,18 +2,76 @@ package wel.wat.edu.pl.mojpiniadz.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import wel.wat.edu.pl.mojpiniadz.R;
 
 public class PersonalDataActivity extends AppCompatActivity {
+
+   private EditText editText1;
+   private Button add;
+   private TextView v;
+
+    private static final String SHARED_PREF_NAME="username";
+    private static final String KEY_NAME="key_username";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_data);
 
-        getSupportActionBar().setTitle("Dane użytkownika");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setTitle("Dane użytkownika");
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        editText1=(EditText)findViewById(R.id.add_name);
+        add=(Button)findViewById(R.id.add_personal_data);
+        v=(TextView)findViewById(R.id.text);
+
+
+        displayName();
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveName();
+
+            }
+        });
+    }
+
+    private void displayName(){
+        SharedPreferences sp=getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
+        String name= sp.getString(KEY_NAME, null);
+
+        if(name != null ){
+            v.setText("Witaj " + name);
+        }
+
+    }
+    private void saveName(){
+        String name=editText1.getText().toString().trim();
+
+        if(name.isEmpty()){
+            Toast.makeText(getApplicationContext(),"Uzupełnij obie informacje!", Toast.LENGTH_SHORT).show();
+        }else {
+            SharedPreferences sp= getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+            SharedPreferences.Editor e=sp.edit();
+            e.putString(KEY_NAME, name);
+            e.apply();
+            editText1.setText("");
+
+
+            displayName();
+        }
     }
 }
+
+
+
